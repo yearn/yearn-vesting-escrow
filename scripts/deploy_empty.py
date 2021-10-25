cad82277d165c7021f176940a4b12a6833e69c91
panda <87183122+pandadefi@users.noreply.github.com>
2021-10-25 12:56:48 +0200

    feat: allow token recipient to collect others token dust

--- scripts/deploy_empty.py
+++ scripts/deploy_empty.py
@@ -1,6 +1,7 @@
 from brownie import VestingEscrowSimple, VestingEscrowFactory, accounts
 
+
 def main():
-    admin = accounts.load('deployer')
+    admin = accounts.load("deployer")
     template = VestingEscrowSimple.deploy({"from": admin})
     factory = VestingEscrowFactory.deploy(template, {"from": admin})
