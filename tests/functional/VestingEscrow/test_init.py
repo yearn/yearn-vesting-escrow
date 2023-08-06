482431bf8e485b248d14aebdf23dd463636b51a6
milkyklim <10698619+milkyklim@users.noreply.github.com>
2023-08-06 13:54:53 +0100

    feat: update init test

--- tests/functional/VestingEscrow/test_init.py
+++ tests/functional/VestingEscrow/test_init.py
@@ -1,7 +1,26 @@
 import ape
 
 
-def test_reinit_impossible(vesting, ychad, token):
-    vesting.renounce_ownership(sender=ychad)
-    with ape.reverts(dev_message="dev: can only initialize once"):
-        vesting.initialize(ychad, token, ychad, 0, 0, 0, 0, sender=ychad)
+def test_vesting_reinit(
+    vesting,
+    ychad,
+    receiver,
+    vesting_factory,
+    token,
+    amount,
+    start_time,
+    cliff_duration,
+    duration,
+):
+    token.approve(vesting_factory, amount, sender=ychad)
+    # same salt reverts
+    with ape.reverts(dev_message="dev: CHECK_NONZERO"):
+        vesting_factory.deploy_vesting_contract(
+            token,
+            receiver,
+            amount,
+            duration,
+            start_time,
+            cliff_duration,
+            sender=ychad,
+        )
