fa522088cd0eb89e95bd693e084930a2045ea39c
milkyklim <10698619+milkyklim@users.noreply.github.com>
2023-08-14 22:49:21 +0100

    feat: add ape scripts for deployment

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,48 +1,87 @@
-from brownie import VestingEscrowSimple, VestingEscrowFactory, ERC20, accounts, chain
-
-YEAR = 365 * 86400
-VESTING_ESCROWS = [
-    {
-        "duration": 3 * YEAR,
-        "start": 1594972885,
-        "cliff": YEAR / 2,
-        "recipients": {
-            accounts[1]: 275 * 10 ** 18,
-            accounts[2]: 140 * 10 ** 18,
-            accounts[3]: 60 * 10 ** 18,
-            accounts[4]: 40 * 10 ** 18,
-            accounts[5]: 25 * 10 ** 18,
-            accounts[6]: 13 * 10 ** 18,
-            accounts[7]: 10 * 10 ** 18,
-        },
-    }
-]
+import click
+import ape
+from ape import networks, project
+from ape.cli import (
+    NetworkBoundCommand,
+    ape_cli_context,
+)
 
 
-def main():
-    admin = accounts[0]
-    token = ERC20.deploy("Yearn Token", "YFI", 18, {"from": admin})
-    template = VestingEscrowSimple.deploy({"from": admin})
-    factory = VestingEscrowFactory.deploy(template, admin, {"from": admin})
+YEAR = int(365.25 * 24 * 60 * 60)
+NUMBER_OF_VESTS = 10
+ESCROWS = {
+    "duration": 3 * YEAR,
+    "start_time": 1650990522,
+    "cliff_duration": YEAR // 3,
+    "open_claim": True,
+    "support_vyper": 100,
+    "recipients": {
+        k.address: 2 ** v * 10 ** 18 for k, v in zip(ape.utils.generate_dev_accounts(), range(NUMBER_OF_VESTS))
+    },
+}
 
-    total_amount = sum(sum(x["recipients"].values()) for x in VESTING_ESCROWS)
-    token._mint_for_testing(total_amount)
-    token.approve(factory, total_amount)
-    for x in VESTING_ESCROWS:
-        for recipient, amount in x["recipients"].items():
-            tx = factory.deploy_vesting_contract(
-                token,
-                recipient,
-                amount,
-                x["duration"],
-                x["start"],
-                x["cliff"],
-            )
-            escrow = VestingEscrowSimple.at(tx.new_contracts[0])
-            assert token.balanceOf(escrow) == amount
-            assert escrow.recipient() == recipient
-            print(f"progress {escrow.unclaimed() / escrow.total_locked():.3%}")
-            print("locked", escrow.locked().to("ether"))
-            print("unclaimed", escrow.unclaimed().to("ether"))
-            escrow.claim({"from": escrow.recipient()})
-            print("tokens of recipient", token.balanceOf(recipient).to("ether"))
+@click.group(short_help="Vesting escrow deployment")
+def cli():
+    pass
+
+
+@cli.command(cls=NetworkBoundCommand)
+@ape_cli_context()
+def deploy(cli_ctx):
+    # check network
+    ecosystem_name = networks.provider.network.ecosystem.name
+    network_name = networks.provider.network.name
+    provider_name = networks.provider.name
+
+    owner = cli_ctx.account_manager.test_accounts[0]
+
+    click.secho(
+        f"You are connected to network '{ecosystem_name}:{network_name}:{provider_name}'.",
+        fg="yellow",
+    )
+    click.secho(f"Deployer is set to '{owner}'.", fg="yellow")
+
+    vyper_donate = "0x70CCBE10F980d80b7eBaab7D2E3A73e87D67B775"
+
+    target = project.VestingEscrowSimple.deploy(sender=owner)
+    factory = project.VestingEscrowFactory.deploy(target, vyper_donate, sender=owner)
+
+    token = project.MockToken.deploy(sender=owner)
+    target = project.VestingEscrowSimple.deploy(sender=owner)
+    factory = project.VestingEscrowFactory.deploy(target, vyper_donate, sender=owner)
+
+    amount = sum(ESCROWS["recipients"].values())
+    support_amount = sum(ESCROWS["recipients"].values()) * ESCROWS["support_vyper"] // 10_000
+    total_amount = amount + support_amount
+
+    token.mint(owner, total_amount, sender=owner)
+    token.approve(factory, total_amount, sender=owner)
+
+    for recipient, amount in ESCROWS["recipients"].items():
+        if recipient == owner:
+            continue
+
+        tx = factory.deploy_vesting_contract(
+            token,
+            recipient,
+            amount,
+            ESCROWS["duration"],
+            ESCROWS["start_time"],
+            ESCROWS["cliff_duration"],
+            ESCROWS["open_claim"],
+            ESCROWS["support_vyper"],
+            sender=owner,
+        )
+
+        escrow = project.VestingEscrowSimple.at(tx.return_value)
+
+        assert token.balanceOf(escrow) == amount
+        assert escrow.recipient() == recipient
+        assert escrow.end_time() == ESCROWS["start_time"] + ESCROWS["duration"]
+        assert escrow.start_time() == ESCROWS["start_time"]
+        assert escrow.cliff_length() == ESCROWS["cliff_duration"]
+        assert escrow.open_claim() == ESCROWS["open_claim"]
+
+        print(f"progress {escrow.unclaimed() / escrow.total_locked():.3%}")
+        print("locked", escrow.locked())
+        print("unclaimed", escrow.unclaimed())
--- scripts/deploy_empty.py
+++ scripts/deploy_empty.py
@@ -1,7 +1,45 @@
-from brownie import VestingEscrowSimple, VestingEscrowFactory, accounts
+import click
+import ape
+from ape.api.networks import LOCAL_NETWORK_NAME
+from ape.types import AddressType
+from ape import networks, project
+from ape.cli import (
+    NetworkBoundCommand,
+    network_option,
+    get_user_selected_account,
+    ape_cli_context,
+)
 
 
-def main():
-    admin = accounts.load("deployer")
-    template = VestingEscrowSimple.deploy({"from": admin})
-    factory = VestingEscrowFactory.deploy(template, {"from": admin})
+@click.group(short_help="Vesting factory and target deployment")
+def cli():
+    pass
+
+
+@cli.command(cls=NetworkBoundCommand)
+@ape_cli_context()
+@network_option()
+def deploy(cli_ctx, network):
+    # check network
+    ecosystem_name = networks.provider.network.ecosystem.name
+    network_name = networks.provider.network.name
+    provider_name = networks.provider.name
+
+    print(network_name)
+
+    if network == LOCAL_NETWORK_NAME or network_name.endswith("-fork"):
+        account = cli_ctx.account_manager.test_accounts[0]
+    else:
+        account = get_user_selected_account()
+
+    click.secho(
+        f"You are connected to network '{ecosystem_name}:{network_name}:{provider_name}'.",
+        fg="yellow",
+    )
+    click.secho(f"Deployer is set to '{account}'.", fg="yellow")
+
+    vyper_donate = ape.convert("vyperlang.eth", AddressType)
+    assert vyper_donate == "0x70CCBE10F980d80b7eBaab7D2E3A73e87D67B775"
+
+    target = project.VestingEscrowSimple.deploy(sender=account)
+    factory = project.VestingEscrowFactory.deploy(target, vyper_donate, sender=account)
